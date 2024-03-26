f6962da591025c441e26d1cbfe21c0ada3ddaadf
ptvty <ptvty@proton.me>
2024-03-26 14:19:31 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.Diagnostics.Tracing.Session;
+
+namespace WinFileReadEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (TraceEventSession.IsElevated() != true)
+            {
+                Console.WriteLine("x|To turn on ETW events you need to be Administrator, please run from an Admin process.");
+                return;
+            }
+
+            string? filePath = (args.Length < 1) ? null : args[0];
+
+            using var session = new TraceEventSession("FileRead");
+
+            Console.CancelKeyPress += (sender, e) => session.Stop();
+
+            session.EnableKernelProvider(
+                KernelTraceEventParser.Keywords.DiskFileIO |
+                KernelTraceEventParser.Keywords.FileIOInit);
+
+            session.Source.Kernel.FileIORead += data =>
+            {
+                if (filePath == null || string.Compare(data.FileName, filePath, StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    string line = ">|";
+                    line += data.Offset + "|";
+                    line += data.IoSize + "|";
+                    line += data.FileName;
+                    Console.WriteLine(line);
+                }
+            };
+
+            session.Source.Process();
+        }
+    }
+}
